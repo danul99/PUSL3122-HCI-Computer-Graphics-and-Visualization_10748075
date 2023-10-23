e9169b43faf8bfda722452400050fc799116fc56
danul99 <73893243+danul99@users.noreply.github.com>
2023-10-23 13:04:59 +0530

    PUSL3122 HCI, Computer Graphics, and Visualization_10748075

---
+++ HCI_10748075/src/Shapes_2D.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Shapes_2D {
+    private JPanel mypanel2;
+    private JButton rectangleButton;
+    private JButton circleButton;
+    private JButton lineButton;
+    private JButton ellipseButton;
+    private JButton arcButton;
+    private JButton roundRectangleButton;
+}
